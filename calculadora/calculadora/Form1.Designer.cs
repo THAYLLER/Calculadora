200c10e198bf71ecff087bc2436fdcb28f568c4c
Thayller <thayllervilelacintra@yahoo.com.br>
2014-07-08 00:00:27 -0300

    Test
    Test

---
+++ calculadora/calculadora/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace calculadora
+{
+    partial class FmrCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TbTela = new System.Windows.Forms.TextBox();
+            this.BtoOito = new System.Windows.Forms.Button();
+            this.BtoSete = new System.Windows.Forms.Button();
+            this.BtoSeis = new System.Windows.Forms.Button();
+            this.BtoCinco = new System.Windows.Forms.Button();
+            this.BtoQuatro = new System.Windows.Forms.Button();
+            this.BtoTres = new System.Windows.Forms.Button();
+            this.BtoDois = new System.Windows.Forms.Button();
+            this.BtoUm = new System.Windows.Forms.Button();
+            this.BtoZero = new System.Windows.Forms.Button();
+            this.BtoNove = new System.Windows.Forms.Button();
+            this.BtoVirgula = new System.Windows.Forms.Button();
+            this.BtoSoma = new System.Windows.Forms.Button();
+            this.BtoSubtrair = new System.Windows.Forms.Button();
+            this.BtoMultiplicar = new System.Windows.Forms.Button();
+            this.BtoDividir = new System.Windows.Forms.Button();
+            this.BtoPonto = new System.Windows.Forms.Button();
+            this.BtoLimpar = new System.Windows.Forms.Button();
+            this.BtoIgual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TbTela
+            // 
+            this.TbTela.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbTela.Location = new System.Drawing.Point(12, 12);
+            this.TbTela.Multiline = true;
+            this.TbTela.Name = "TbTela";
+            this.TbTela.ReadOnly = true;
+            this.TbTela.Size = new System.Drawing.Size(426, 33);
+            this.TbTela.TabIndex = 0;
+            // 
+            // BtoOito
+            // 
+            this.BtoOito.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoOito.Location = new System.Drawing.Point(84, 51);
+            this.BtoOito.Name = "BtoOito";
+            this.BtoOito.Size = new System.Drawing.Size(66, 56);
+            this.BtoOito.TabIndex = 1;
+            this.BtoOito.Text = "8";
+            this.BtoOito.UseVisualStyleBackColor = true;
+            this.BtoOito.Click += new System.EventHandler(this.BtoOito_Click);
+            // 
+            // BtoSete
+            // 
+            this.BtoSete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoSete.Location = new System.Drawing.Point(156, 51);
+            this.BtoSete.Name = "BtoSete";
+            this.BtoSete.Size = new System.Drawing.Size(66, 56);
+            this.BtoSete.TabIndex = 1;
+            this.BtoSete.Text = "7";
+            this.BtoSete.UseVisualStyleBackColor = true;
+            this.BtoSete.Click += new System.EventHandler(this.BtoSete_Click);
+            // 
+            // BtoSeis
+            // 
+            this.BtoSeis.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoSeis.Location = new System.Drawing.Point(12, 113);
+            this.BtoSeis.Name = "BtoSeis";
+            this.BtoSeis.Size = new System.Drawing.Size(66, 56);
+            this.BtoSeis.TabIndex = 1;
+            this.BtoSeis.Text = "6";
+            this.BtoSeis.UseVisualStyleBackColor = true;
+            this.BtoSeis.Click += new System.EventHandler(this.BtoSeis_Click);
+            // 
+            // BtoCinco
+            // 
+            this.BtoCinco.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoCinco.Location = new System.Drawing.Point(84, 113);
+            this.BtoCinco.Name = "BtoCinco";
+            this.BtoCinco.Size = new System.Drawing.Size(66, 56);
+            this.BtoCinco.TabIndex = 1;
+            this.BtoCinco.Text = "5";
+            this.BtoCinco.UseVisualStyleBackColor = true;
+            this.BtoCinco.Click += new System.EventHandler(this.BtoCinco_Click);
+            // 
+            // BtoQuatro
+            // 
+            this.BtoQuatro.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoQuatro.Location = new System.Drawing.Point(156, 113);
+            this.BtoQuatro.Name = "BtoQuatro";
+            this.BtoQuatro.Size = new System.Drawing.Size(66, 56);
+            this.BtoQuatro.TabIndex = 1;
+            this.BtoQuatro.Text = "4";
+            this.BtoQuatro.UseVisualStyleBackColor = true;
+            this.BtoQuatro.Click += new System.EventHandler(this.BtoQuatro_Click);
+            // 
+            // BtoTres
+            // 
+            this.BtoTres.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoTres.Location = new System.Drawing.Point(12, 175);
+            this.BtoTres.Name = "BtoTres";
+            this.BtoTres.Size = new System.Drawing.Size(66, 56);
+            this.BtoTres.TabIndex = 1;
+            this.BtoTres.Text = "3";
+            this.BtoTres.UseVisualStyleBackColor = true;
+            this.BtoTres.Click += new System.EventHandler(this.BtoTres_Click);
+            // 
+            // BtoDois
+            // 
+            this.BtoDois.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoDois.Location = new System.Drawing.Point(84, 175);
+            this.BtoDois.Name = "BtoDois";
+            this.BtoDois.Size = new System.Drawing.Size(66, 56);
+            this.BtoDois.TabIndex = 1;
+            this.BtoDois.Text = "2";
+            this.BtoDois.UseVisualStyleBackColor = true;
+            this.BtoDois.Click += new System.EventHandler(this.BtoDois_Click);
+            // 
+            // BtoUm
+            // 
+            this.BtoUm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoUm.Location = new System.Drawing.Point(156, 175);
+            this.BtoUm.Name = "BtoUm";
+            this.BtoUm.Size = new System.Drawing.Size(66, 56);
+            this.BtoUm.TabIndex = 1;
+            this.BtoUm.Text = "1";
+            this.BtoUm.UseVisualStyleBackColor = true;
+            this.BtoUm.Click += new System.EventHandler(this.BtoUm_Click);
+            // 
+            // BtoZero
+            // 
+            this.BtoZero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoZero.Location = new System.Drawing.Point(228, 51);
+            this.BtoZero.Name = "BtoZero";
+            this.BtoZero.Size = new System.Drawing.Size(66, 118);
+            this.BtoZero.TabIndex = 1;
+            this.BtoZero.Text = "0";
+            this.BtoZero.UseVisualStyleBackColor = true;
+            this.BtoZero.Click += new System.EventHandler(this.BtoZero_Click);
+            // 
+            // BtoNove
+            // 
+            this.BtoNove.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoNove.Location = new System.Drawing.Point(12, 51);
+            this.BtoNove.Name = "BtoNove";
+            this.BtoNove.Size = new System.Drawing.Size(66, 56);
+            this.BtoNove.TabIndex = 1;
+            this.BtoNove.Text = "9";
+            this.BtoNove.UseVisualStyleBackColor = true;
+            this.BtoNove.Click += new System.EventHandler(this.BtoNove_Click);
+            // 
+            // BtoVirgula
+            // 
+            this.BtoVirgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoVirgula.Location = new System.Drawing.Point(228, 175);
+            this.BtoVirgula.Name = "BtoVirgula";
+            this.BtoVirgula.Size = new System.Drawing.Size(66, 56);
+            this.BtoVirgula.TabIndex = 1;
+            this.BtoVirgula.Text = ",";
+            this.BtoVirgula.UseVisualStyleBackColor = true;
+            this.BtoVirgula.Click += new System.EventHandler(this.BtoVirgula_Click);
+            // 
+            // BtoSoma
+            // 
+            this.BtoSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoSoma.Location = new System.Drawing.Point(300, 113);
+            this.BtoSoma.Name = "BtoSoma";
+            this.BtoSoma.Size = new System.Drawing.Size(66, 56);
+            this.BtoSoma.TabIndex = 1;
+            this.BtoSoma.Text = "+";
+            this.BtoSoma.UseVisualStyleBackColor = true;
+            this.BtoSoma.Click += new System.EventHandler(this.BtoSoma_Click);
+            // 
+            // BtoSubtrair
+            // 
+            this.BtoSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoSubtrair.Location = new System.Drawing.Point(300, 175);
+            this.BtoSubtrair.Name = "BtoSubtrair";
+            this.BtoSubtrair.Size = new System.Drawing.Size(66, 56);
+            this.BtoSubtrair.TabIndex = 1;
+            this.BtoSubtrair.Text = "-";
+            this.BtoSubtrair.UseVisualStyleBackColor = true;
+            this.BtoSubtrair.Click += new System.EventHandler(this.BtoSubtrair_Click);
+            // 
+            // BtoMultiplicar
+            // 
+            this.BtoMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoMultiplicar.Location = new System.Drawing.Point(372, 51);
+            this.BtoMultiplicar.Name = "BtoMultiplicar";
+            this.BtoMultiplicar.Size = new System.Drawing.Size(66, 56);
+            this.BtoMultiplicar.TabIndex = 1;
+            this.BtoMultiplicar.Text = "*";
+            this.BtoMultiplicar.UseVisualStyleBackColor = true;
+            this.BtoMultiplicar.Click += new System.EventHandler(this.BtoMultiplicar_Click);
+            // 
+            // BtoDividir
+            // 
+            this.BtoDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoDividir.Location = new System.Drawing.Point(372, 113);
+            this.BtoDividir.Name = "BtoDividir";
+            this.BtoDividir.Size = new System.Drawing.Size(66, 56);
+            this.BtoDividir.TabIndex = 1;
+            this.BtoDividir.Text = "/";
+            this.BtoDividir.UseVisualStyleBackColor = true;
+            this.BtoDividir.Click += new System.EventHandler(this.BtoDividir_Click);
+            // 
+            // BtoPonto
+            // 
+            this.BtoPonto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoPonto.Location = new System.Drawing.Point(300, 51);
+            this.BtoPonto.Name = "BtoPonto";
+            this.BtoPonto.Size = new System.Drawing.Size(66, 56);
+            this.BtoPonto.TabIndex = 1;
+            this.BtoPonto.Text = ".";
+            this.BtoPonto.UseVisualStyleBackColor = true;
+            this.BtoPonto.Click += new System.EventHandler(this.BtoPonto_Click);
+            // 
+            // BtoLimpar
+            // 
+            this.BtoLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoLimpar.Location = new System.Drawing.Point(372, 175);
+            this.BtoLimpar.Name = "BtoLimpar";
+            this.BtoLimpar.Size = new System.Drawing.Size(66, 56);
+            this.BtoLimpar.TabIndex = 1;
+            this.BtoLimpar.Text = "C";
+            this.BtoLimpar.UseVisualStyleBackColor = true;
+            this.BtoLimpar.Click += new System.EventHandler(this.BtoLimpar_Click);
+            // 
+            // BtoIgual
+            // 
+            this.BtoIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtoIgual.Location = new System.Drawing.Point(12, 237);
+            this.BtoIgual.Name = "BtoIgual";
+            this.BtoIgual.Size = new System.Drawing.Size(426, 56);
+            this.BtoIgual.TabIndex = 1;
+            this.BtoIgual.Text = "=";
+            this.BtoIgual.UseVisualStyleBackColor = true;
+            this.BtoIgual.Click += new System.EventHandler(this.BtoIgual_Click);
+            // 
+            // FmrCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 302);
+            this.Controls.Add(this.BtoIgual);
+            this.Controls.Add(this.BtoLimpar);
+            this.Controls.Add(this.BtoDividir);
+            this.Controls.Add(this.BtoPonto);
+            this.Controls.Add(this.BtoSoma);
+            this.Controls.Add(this.BtoMultiplicar);
+            this.Controls.Add(this.BtoVirgula);
+            this.Controls.Add(this.BtoZero);
+            this.Controls.Add(this.BtoSubtrair);
+            this.Controls.Add(this.BtoNove);
+            this.Controls.Add(this.BtoTres);
+            this.Controls.Add(this.BtoSeis);
+            this.Controls.Add(this.BtoUm);
+            this.Controls.Add(this.BtoQuatro);
+            this.Controls.Add(this.BtoSete);
+            this.Controls.Add(this.BtoDois);
+            this.Controls.Add(this.BtoCinco);
+            this.Controls.Add(this.BtoOito);
+            this.Controls.Add(this.TbTela);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "FmrCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbTela;
+        private System.Windows.Forms.Button BtoOito;
+        private System.Windows.Forms.Button BtoSete;
+        private System.Windows.Forms.Button BtoSeis;
+        private System.Windows.Forms.Button BtoCinco;
+        private System.Windows.Forms.Button BtoQuatro;
+        private System.Windows.Forms.Button BtoTres;
+        private System.Windows.Forms.Button BtoDois;
+        private System.Windows.Forms.Button BtoUm;
+        private System.Windows.Forms.Button BtoZero;
+        private System.Windows.Forms.Button BtoNove;
+        private System.Windows.Forms.Button BtoVirgula;
+        private System.Windows.Forms.Button BtoSoma;
+        private System.Windows.Forms.Button BtoSubtrair;
+        private System.Windows.Forms.Button BtoMultiplicar;
+        private System.Windows.Forms.Button BtoDividir;
+        private System.Windows.Forms.Button BtoPonto;
+        private System.Windows.Forms.Button BtoLimpar;
+        private System.Windows.Forms.Button BtoIgual;
+    }
+}
+
---
+++ calculadora/calculadora/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora
+{
+    public partial class FmrCalculadora : Form
+    {
+        public FmrCalculadora()
+        {
+            InitializeComponent();
+        }
+        bool detecta = true;
+        double valor1, valor2, resultado;
+        string operacao;
+        private void BtoZero_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 0);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 0);
+            }
+        }
+
+        private void BtoUm_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 1);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 1);
+            }
+        }
+
+        private void BtoDois_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 2);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 2);
+            }
+        }
+
+        private void BtoTres_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 3);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 3);
+            }
+        }
+
+        private void BtoQuatro_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 4);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 4);
+            }
+        }
+
+        private void BtoCinco_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 5);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 5);
+            }
+        }
+
+        private void BtoSeis_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 6);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 6);
+            }
+        }
+
+        private void BtoSete_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 7);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 7);
+            }
+        }
+
+        private void BtoOito_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 8);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 8);
+            }
+        }
+
+        private void BtoNove_Click(object sender, EventArgs e)
+        {
+            if (detecta)
+            {
+                TbTela.Text = "";
+                valor1 = Convert.ToDouble(TbTela.Text += 9);
+                detecta = false;
+            }
+            else
+            {
+                valor1 = Convert.ToDouble(TbTela.Text += 9);
+            }
+        }
+
+        private void BtoVirgula_Click(object sender, EventArgs e)
+        {
+            TbTela.Text += ",";
+            detecta = false;
+        }
+
+        private void BtoPonto_Click(object sender, EventArgs e)
+        {
+            TbTela.Text += ".";
+            detecta = false;
+        }
+
+        private void BtoLimpar_Click(object sender, EventArgs e)
+        {
+            TbTela.Text = "";
+            detecta = true;
+        }
+
+        private void BtoSoma_Click(object sender, EventArgs e)
+        {
+            operacao = "+";
+            detecta = true;
+            valor2 = Convert.ToDouble(TbTela.Text);
+        }
+
+        private void BtoSubtrair_Click(object sender, EventArgs e)
+        {
+            operacao = "-";
+            detecta = true;
+            valor2 = Convert.ToDouble(TbTela.Text);
+        }
+
+        private void BtoMultiplicar_Click(object sender, EventArgs e)
+        {
+            operacao = "*";
+            detecta = true;
+            valor2 = Convert.ToDouble(TbTela.Text);
+        }
+
+        private void BtoDividir_Click(object sender, EventArgs e)
+        {
+            operacao = "/";
+            detecta = true;
+            valor2 = Convert.ToDouble(TbTela.Text);
+        }
+
+        private void BtoIgual_Click(object sender, EventArgs e)
+        {
+            switch (operacao)
+            {
+                case "+":
+                    {
+                        resultado = valor1 + valor2;
+                        TbTela.Text = resultado.ToString();
+                        break;
+                    }
+
+                case "-":
+                    {
+                        resultado = valor1 - valor2;
+                        TbTela.Text = resultado.ToString();
+                        break;
+                    }
+                case "/":
+                    {
+                        resultado = valor1 / valor2;
+                        TbTela.Text = resultado.ToString();
+                        break;
+                    }
+                case "*":
+                    {
+                        resultado = valor1 * valor2;
+                        TbTela.Text = resultado.ToString();
+                        break;
+                    }
+            }
+        }
+    }
+}
